7b9f52cdad8ef05b73d87db9774ba4e9ed606494
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2024-05-21 19:33:54 +0530

    Initial setup

---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'Packaged - Simple Python packaging';
+export const SITE_DESCRIPTION = 'The easiest way to ship Python applications.';
+export const SITE_URL = 'https://packaged.live';
+export const SITE_IMAGE = '/preview.png';
